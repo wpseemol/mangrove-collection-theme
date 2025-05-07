ab2b130b973fcf174ee1ba68af574bf81f596805
Seemol Chakroborti <wpseemol@gmail.com>
2025-05-07 12:27:31 +0600

    headr after body tage set wp_body_open fucntion

--- footer.php
+++ footer.php
@@ -6,7 +6,19 @@
  */
 ?>
 
-
-
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+        <nav class="footer-nav">
+            <?php
+                wp_nav_menu( array(
+                    'theme_location' => 'footer-menu',
+                    'container'      => false,
+                    'items_wrap'     => '<ul>%3$s</ul>',
+                ) );
+            ?>
+        </nav> 
+    </footer>
+</main>
+<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -15,7 +15,11 @@
     <meta name="author" content="<?php bloginfo( 'name' ); ?>">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <?php wp_head(); ?>
 </head>
 <body>
+    <?php wp_body_open(); ?>
+
+<main>
     
